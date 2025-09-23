590778ab92358df0592d39700e59fb3180cf91b3
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-22 22:11:29 -0500

    feat: Add state to Hero component

--- src/components/modules/Hero.jsx
+++ src/components/modules/Hero.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Hero = () => {
+  const [like, setLike] = useState(0);
+
   return (
     <article className="px-4 py-5 my-5 text-center">
       <div className="container">
@@ -8,10 +12,13 @@ const Hero = () => {
             Welcome to Happy Store, a simple e-commerce website built with React and Bootstrap. 🚀
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button"
-              className="btn btn-primary px-4 gap-3">Products</button>
-            <button type="button"
-              className="btn btn-outline-secondary px-4">Outlet</button>
+            <button className="btn btn-primary px-4 gap-3">Products</button>
+            <button
+              className="btn btn-outline-secondary px-4"
+              onClick={() => setLike(like + 1)}
+            >
+              Like ({like})
+            </button>
           </div>
         </div>
       </div>

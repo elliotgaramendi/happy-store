0944e13a11897d076fe545ebba083e31f298b174
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-22 20:38:36 -0500

    feat: Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { data } from "./data/data";
+
 function App() {
+  console.log(data);
+
   return (
-    <h1>
-      Happy Store
-    </h1>
+    <>
+      <section className="py-5">
+        <div className="container text-center">
+          <h1>Happy Store</h1>
+          <p>Welcome to Happy Store</p>
+        </div>
+      </section>
+      <section className="py-4">
+        <div className="container">
+          <h2>Products</h2>
+          <ul>
+            {data.map(item => {
+              const {id, title} = item;
+              return (
+                <li key={id}>{title}</li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
   )
 }
 

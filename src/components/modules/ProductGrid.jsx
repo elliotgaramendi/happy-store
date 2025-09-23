ddd6f5d9735f9642d81be8fb52aced48b23efe1c
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-22 21:53:56 -0500

    feat: Add ProductGrid component

---
+++ src/components/modules/ProductGrid.jsx
@@ -0,0 +1,28 @@
+const ProductGrid = ({ data }) => {
+  return (
+    <section className="py-4">
+      <div className="container">
+        <h2>Products</h2>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+          {data.map(item => {
+            const { id, title, price, description, category, image, rating } = item;
+            return (
+              <div className="col" key={id}>
+                <div className="card">
+                  <img src={image} className="card-img-top object-fit-contain" alt={title} height={200} />
+                  <div className="card-body">
+                    <h4 className="card-title">{title} <span className="h6">{rating.rate} ⭐</span></h4>
+                    <h5 className="card-subtitle mb-2 text-body-secondary">{category}</h5>
+                    <p className="card-text">{description.slice(0, 128)}...</p>
+                    <h5>${price}</h5>
+                  </div>
+                </div>
+              </div>)
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductGrid;

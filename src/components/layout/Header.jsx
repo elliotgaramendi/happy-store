bc2d94950737d284a56b22da658b300a66fb7630
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 20:59:04 -0500

    feat: update header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,22 +1,24 @@
+import { Link, NavLink } from "react-router";
+
 const Header = () => {
   return (
     <header className="fixed-top">
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container">
-          <a className="navbar-brand" href="#">HS 🛒</a>
+          <Link className="navbar-brand" to="/">HS 🛒</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="#">Products</a>
+                <NavLink className="nav-link" to="/products">Products</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Promotions</a>
+                <NavLink className="nav-link" to="/promotions">Promotions</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <NavLink className="nav-link" to="/contact">Contact</NavLink>
               </li>
             </ul>
             <form className="d-flex" role="search">

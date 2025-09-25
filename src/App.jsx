1f013aa808aec90eba5937c0151969097ab0c257
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 20:21:10 -0500

    feat: update data && add Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
-import Hero from "./components/modules/Hero";
-import ProductGrid from "./components/modules/ProductGrid";
-import { data } from "./data/data";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
       <main className="mt-5">
-        <Hero />
-        <ProductGrid data={data} />
+        <Home />
       </main>
       <Footer />
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from "../components/modules/Hero";
+import ProductGrid from "../components/modules/ProductGrid";
+import { products } from "../data/fakeStore.data";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <ProductGrid data={products} />
+    </>
+  );
+}
+
+export default Home;

1ef9bbf944b54c7c6de8f7fc571fd1e7ec11599d
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 20:50:05 -0500

    feat: add Contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,55 @@
+const Contact = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <div className="row justify-content-center">
+              <div className="col-md-6">
+                <h3 className="heading mb-4">Let's talk about everything!</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas debitis, fugit natus?</p>
+                <p><img src="images/undraw-contact.svg" alt="Image" className="img-fluid" /></p>
+              </div>
+              <div className="col-md-6">
+                <form className="mb-5" method="post" id="contactForm" name="contactForm" novalidate="novalidate">
+                  <div className="row">
+                    <div className="col-md-12 form-group">
+                      <input type="text" className="form-control" name="name" id="name" placeholder="Your name" />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12 form-group">
+                      <input type="text" className="form-control" name="email" id="email" placeholder="Email" />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12 form-group">
+                      <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12 form-group">
+                      <textarea className="form-control" name="message" id="message" cols="30" rows="7" placeholder="Write your message"></textarea>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-12">
+                      <input type="submit" value="Send Message" className="btn btn-primary rounded-0 py-2 px-4" />
+                      <span className="submitting"></span>
+                    </div>
+                  </div>
+                </form>
+                <div id="form-message-warning mt-4"></div>
+                <div id="form-message-success">
+                  Your message was sent, thank you!
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import Contact from "../pages/Contact";
 import Home from "../pages/Home";
 
 const AppRoutes = () => {
@@ -7,7 +8,7 @@ const AppRoutes = () => {
       <Route index element={<Home />} />
       <Route path="products" element={<h1>Products</h1>} />
       <Route path="promotions" element={<h1>Promotions</h1>} />
-      <Route path="contact" element={<h1>Contact</h1>} />
+      <Route path="contact" element={<Contact />} />
     </Routes>
   );
 };
